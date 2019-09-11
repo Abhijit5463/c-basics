0f17562d937b0b02ae4cfd7d574450301b4aa0c8
Abhijit5463 <55055946+Abhijit5463@users.noreply.github.com>
2019-09-12 01:23:27 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{	
+int a;
+int b;
+int c;
+printf("enter the value of a=");
+scanf("%d,&a");
+printf("enter the value of b=");
+scanf("%d,&b");
+c=a+b;
+printf("the sum is");
+scanf("%d,&c;sum");
+return 0;
+getch();
+}
